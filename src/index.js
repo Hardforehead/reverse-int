22f1d6457d5632fbcbc6e91bf4179faf520e5193
Hardforehead <kochegar005@gmail.com>
2022-07-06 18:49:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+    let a = '';
+    n = Math.abs(n);
+    while (n > 0) {
+    a = a + (n % 10);
+    n = parseInt(n / 10);
+    }
+    
+    return a;
+  }
